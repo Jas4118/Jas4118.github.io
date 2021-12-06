e0bac38326c9989eacf57452baebd077821837a8
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2021-12-06 07:44:09 -0500

    Update ui.js

--- Weather Info Lookup/ui.js
+++ Weather Info Lookup/ui.js
@@ -4,12 +4,11 @@ class UI {
       this.city;
       this.defaultCity = "New York";
     }
-  
+    #function that takes in parameter got from fetch and parses
     populateUI(data) {
   
       this.uiContainer.innerHTML = `        
           <div>
-            units=imperial
               <div>
                   <h3> ${data.name}</h3>
                   <h4> High: ${data.main.temp_max}. Low: ${data.main.temp_min}</h4>
