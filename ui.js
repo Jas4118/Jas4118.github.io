2caf503f3b1c3949f93bedc68e5fda50d6d06db8
Jas4118 <70446078+Jas4118@users.noreply.github.com>
2023-11-01 02:43:07 -0400

    Update ui.js

--- ui.js
+++ ui.js
@@ -11,7 +11,7 @@ class UI {
           <div>
               <div>
                   <h3> ${data.name}</h3>
-                  <h4> High: ${data.main.temp_max}. Low: ${data.main.temp_min}</h4>
+                  <h4> High: ${data.main.temp_max}°F Low: ${data.main.temp_min}°F</h4>
                   <h5>It currently feels like ${data.main.feels_like} and the humidity is at ${data.main.humidity} in ${data.name}.</h5>              
               </div>
           </div>        
